b0fc4837d8e7f73e85340526b694b06e29117d1b
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-05-16 23:08:09 -0700

    Update links.js

--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -1,15 +1,15 @@
-import { FaWpforms } from "react-icons/fa";
-import { ImProfile } from "react-icons/im";
-import { IoHome } from "react-icons/io5";
-import { FaHeadphones } from "react-icons/fa";
+import { BsPlusSquare } from "react-icons/bs";
+import { RiUserFill } from "react-icons/ri";
+import { IoHomeOutline } from "react-icons/io5";
+import { IoCallOutline } from "react-icons/io5";
+import { TfiBook } from "react-icons/tfi";
 
 const links = [
-  { id: 1, text: "feed", path: "/", icon: <IoHome /> },
-  { id: 2, text: "daily logs", path: "daily-logs", icon: <FaWpforms /> },
-  { id: 3, text: "add story", path: "add-story", icon: <FaWpforms /> },
-  { id: 4, text: "profile", path: "profile", icon: <ImProfile /> },
-  { id: 5, text: "resources", path: "resources", icon: <FaHeadphones /> },
-  
+  { id: 1, text: "feed", path: "/", icon: <IoHomeOutline /> },
+  { id: 2, text: "daily logs", path: "daily-logs", icon: <TfiBook /> },
+  { id: 3, text: "post", path: "add-story", icon: <BsPlusSquare /> },
+  { id: 4, text: "profile", path: "profile", icon: <RiUserFill /> },
+  { id: 5, text: "resources", path: "resources", icon: <IoCallOutline /> },
 ];
 
 export default links;
