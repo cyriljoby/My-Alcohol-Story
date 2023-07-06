02d08c22ad4ab8c48b41ca9f685024d04a88d85b
Anit Annadi <72627877+AnitAnnadi@users.noreply.github.com>
2023-07-06 14:23:05 -0700

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -99,7 +99,7 @@ const Register = () => {
           submit
         </button>
         <p>
-          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          {values.isMember ? 'Don't have an account?' : 'Already have an account?'}
           <button type='button' onClick={toggleMember} className='member-btn'>
             {values.isMember ? 'Register' : 'Login'}
           </button>
